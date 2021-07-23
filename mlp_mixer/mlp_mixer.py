4a08e8a90d9783ddfcfc8bad6917d62916d1f45f
Sebastian <mocart15@gmail.com>
2021-07-23 12:19:44 +0200

    Refactored models code to resemble keras.applications more.

--- mlp_mixer/mlp_mixer.py
+++ mlp_mixer/mlp_mixer.py
@@ -1,4 +1,6 @@
 """MLP-Mixer models, reimplemented in Keras."""
+import math
+from typing import Tuple, Union
 
 import tensorflow as tf
 from keras.applications import imagenet_utils
@@ -13,15 +15,41 @@ POSSIBLE_WEIGHT_VARIANTS = {
 }
 
 
+def _validate_input_shape(
+    input_shape: Union[None, Tuple[int, int, int]],
+    default_shape: Tuple[int, int, int],
+    minimum_shape: int,
+    pretrained: bool,
+):
+    if input_shape is None:
+        return default_shape
+
+    if pretrained:
+        if input_shape != default_shape:
+            raise ValueError(
+                "When passing weights `imagenet` or `sam` input shape must be"
+                f"equal to default input shape: {default_shape} or `None`."
+            )
+    else:
+        if len(input_shape) != 3:
+            raise ValueError("Input shape must be tuple of 3 integer.")
+        elif input_shape[-1] not in {1, 3}:
+            raise ValueError("Input shape must have 1 or 3 channels in the last dim.")
+        elif input_shape[0] < minimum_shape or input_shape[1] < minimum_shape:
+            raise ValueError(f"Minimum value for input height/width is {minimum_shape}")
+        else:
+            return input_shape
+
+
 def MLPMixer(
     num_blocks: int,
     patch_size: int,
     hidden_size: int,
     tokens_mlp_dim: int,
     channels_mlp_dim: int,
-    default_size: int,
     dropout_rate=0.2,
     epsilon=1e-06,
+    default_shape: Tuple[int, int, int] = (224, 224, 3),
     name="",
     include_top=True,
     weights="imagenet",
@@ -33,7 +61,34 @@ def MLPMixer(
 ):
     """Create MLP-Mixer model, based on provided parameters.
 
-    # TODO: extend this docstring, with parameter description.
+    :arg num_blocks: how many times to repeat mixer block in the model architecture.
+    :arg patch_size: how big (in pixels) the extracted patches should be.
+    :arg hidden_size: number of filters in STEM conv2d layer.
+    :arg tokens_mlp_dim: number of dense layer units in token-mixing part of the block.
+    :arg channels_mlp_dim: number of dense layer units in channel-mixing part of the
+        block.
+    :arg dropout_rate: at what rate to apply dropout to layers. This was not present
+        in the original repo, but I added it as a regularization technique.
+    :arg epsilon: epsilon argument for LayerNormalization.
+    :arg default_shape: shape that the network has been trained with, in the original
+        repository. Also, for this shape the official weights have been released.
+
+    :arg name: name of the model. Useful to determine which model variant is currently
+        used.
+    :arg include_top: whether to include avg_pooling + prediction head.
+        Defaults to False.
+    :arg weights: path to .h5 file or "imagenet" or "sam" (depending on the model
+        variant). Not all combinations variant-weights have been released. Defaults to
+        `imagenet` or `sam`, if imagenet is not available.
+    :arg input_tensor: optional Keras tensor (i.e. output of `layers.Input()`) to use
+        as image input for the model. Defaults to None.
+    :arg input_shape: input shape to build the model with. Defaults to None.
+    :arg pooling: only works when `include_top=False`. Which pooling to apply after last
+        block and normalization. Can be one of {"avg", "max" or None}. Defaults to None.
+    :arg classes: only works when include_top=False. Number of classes for prediction
+        head. Defaults to 1000.
+    :arg classifier_activation: function to apply to classifier head. Defaults to
+        "softmax".
     """
     # Handle weights
     model_variant = name.split("-")[-1]  # b16 or b32 or l16
@@ -53,13 +108,11 @@ def MLPMixer(
         )
 
     # Determine proper input shape
-    input_shape = imagenet_utils.obtain_input_shape(
-        input_shape,
-        default_size=default_size,
-        min_size=32,
-        data_format=backend.image_data_format(),
-        require_flatten=include_top,
-        weights=weights,
+    input_shape = _validate_input_shape(
+        input_shape=input_shape,
+        default_shape=default_shape,
+        pretrained=weights in ["imagenet", "sam"],
+        minimum_shape=patch_size,
     )
 
     # Define input
@@ -97,8 +150,12 @@ def MLPMixer(
         x
     )
 
+    # Reshape output from [batch_size, N, M] to [batch_size, sqrt(N), sqrt(N), M]:
+    feature_size = int(math.sqrt(x.shape[-2]))
+    x = tf.keras.layers.Reshape(target_shape=(feature_size, feature_size, -1))(x)
+
     if include_top:
-        x = tf.keras.layers.GlobalAveragePooling1D(name="avg_pool")(x)
+        x = tf.keras.layers.GlobalAveragePooling2D(name="avg_pool")(x)
         if dropout_rate > 0:
             x = tf.keras.layers.Dropout(dropout_rate, name="top_dropout")(x)
         imagenet_utils.validate_activation(classifier_activation, weights)
@@ -110,9 +167,9 @@ def MLPMixer(
         )(x)
     else:
         if pooling == "avg":
-            x = tf.keras.layers.GlobalAveragePooling1D(name="avg_pool")(x)
+            x = tf.keras.layers.GlobalAveragePooling2D(name="avg_pool")(x)
         elif pooling == "max":
-            x = tf.keras.layers.GlobalMaxPooling1D(name="max_pool")(x)
+            x = tf.keras.layers.GlobalMaxPooling2D(name="max_pool")(x)
 
     # Ensure that the model takes into account
     # any potential predecessors of `input_tensor`.
@@ -194,7 +251,7 @@ def MLPMixer_B16(
         hidden_size=768,
         tokens_mlp_dim=384,
         channels_mlp_dim=3072,
-        default_size=224,
+        default_shape=(224, 224, 3),
         name="mlp-mixer-b16",
         include_top=include_top,
         weights=weights,
@@ -223,7 +280,7 @@ def MLPMixer_B32(
         tokens_mlp_dim=384,
         channels_mlp_dim=3072,
         name="mlp-mixer-b32",
-        default_size=224,
+        default_shape=(224, 224, 3),
         include_top=include_top,
         weights=weights,
         input_tensor=input_tensor,
@@ -251,7 +308,7 @@ def MLPMixer_L16(
         tokens_mlp_dim=512,
         channels_mlp_dim=4096,
         name="mlp-mixer-l16",
-        default_size=224,
+        default_shape=(224, 224, 3),
         include_top=include_top,
         weights=weights,
         input_tensor=input_tensor,
