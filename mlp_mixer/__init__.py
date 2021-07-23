556de0919019778a09c2b82270ee857f316ef5b5
Sebastian <mocart15@gmail.com>
2021-07-23 15:18:59 +0200

    Refactored code to create installable package.

---
+++ mlp_mixer/__init__.py
@@ -0,0 +1,5 @@
+"""Package for importing MLP-Mixer models."""
+
+from mlp_mixer.mlp_mixer import MLPMixer_B16, MLPMixer_B32, MLPMixer_L16
+
+__all__ = ["MLPMixer_B16", "MLPMixer_B32", "MLPMixer_L16"]
