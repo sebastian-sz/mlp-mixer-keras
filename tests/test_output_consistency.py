a282e53fc22b7bb904091ab6654f67fceb3cdda8
Sebastian <mocart15@gmail.com>
2021-07-23 23:05:37 +0200

    Refactored consistency tests.

--- tests/test_output_consistency.py
+++ tests/test_output_consistency.py
@@ -7,15 +7,12 @@ from absl.testing import absltest, parameterized
 
 from mlp_mixer.mlp_mixer import MLPMixer_B16, MLPMixer_B32, MLPMixer_L16
 from tests._root_dir import ROOT_DIR
-
-INPUT_SHAPE = (224, 224)
-WEIGHTS_DIR = "/".join(ROOT_DIR.split("/")[:-1]) + "/weights"
+from tests.test_models import INPUT_SHAPE, TEST_PARAMS
 
 CONSISTENCY_TEST_PARAMS = [
     {
         "testcase_name": "b16-imagenet1k",
         "model_fn": MLPMixer_B16,
-        "weights_path": os.path.join(WEIGHTS_DIR, "mlp-mixer-b16.h5"),
         "original_outputs": os.path.join(
             ROOT_DIR, "assets/original_outputs/b16_imagenet1k.npy"
         ),
@@ -23,7 +20,6 @@ CONSISTENCY_TEST_PARAMS = [
     {
         "testcase_name": "b16-sam",
         "model_fn": MLPMixer_B16,
-        "weights_path": os.path.join(WEIGHTS_DIR, "mlp-mixer-b16-sam.h5"),
         "original_outputs": os.path.join(
             ROOT_DIR, "assets/original_outputs/b16_sam.npy"
         ),
@@ -31,7 +27,6 @@ CONSISTENCY_TEST_PARAMS = [
     {
         "testcase_name": "b32-sam",
         "model_fn": MLPMixer_B32,
-        "weights_path": os.path.join(WEIGHTS_DIR, "mlp-mixer-b32-sam.h5"),
         "original_outputs": os.path.join(
             ROOT_DIR, "assets/original_outputs/b32_sam.npy"
         ),
@@ -39,7 +34,6 @@ CONSISTENCY_TEST_PARAMS = [
     {
         "testcase_name": "l16-imagenet1k",
         "model_fn": MLPMixer_L16,
-        "weights_path": os.path.join(WEIGHTS_DIR, "mlp-mixer-l16.h5"),
         "original_outputs": os.path.join(
             ROOT_DIR, "assets/original_outputs/l16_imagenet1k.npy"
         ),
@@ -53,16 +47,10 @@ class TestKerasVSOriginalOutputConsistency(parameterized.TestCase):
     image = tf.expand_dims(image, axis=0)
 
     @parameterized.named_parameters(CONSISTENCY_TEST_PARAMS)
-    def test_output_consistency(
-        self, model_fn: Callable, weights_path: str, original_outputs: str
-    ):
-        if not os.path.exists(weights_path):
-            self.skipTest("No weights present in repo. Skipping... .")
+    def test_output_consistency(self, model_fn: Callable, original_outputs: str):
+        model = model_fn()
 
-        model = model_fn(weights=None)
-        model.load_weights(weights_path)
-
-        input_tensor = tf.image.resize(self.image, INPUT_SHAPE)
+        input_tensor = tf.image.resize(self.image, INPUT_SHAPE[:2])
         input_tensor = self._pre_process_image(input_tensor)
 
         output = model(input_tensor, training=False)
@@ -73,6 +61,18 @@ class TestKerasVSOriginalOutputConsistency(parameterized.TestCase):
     @staticmethod
     def _pre_process_image(img: tf.Tensor) -> tf.Tensor:
         return (img / 128) - 1
+
+    @parameterized.named_parameters(TEST_PARAMS)
+    def test_feature_extraction_with_pretrained_weights(self, model_fn: Callable):
+        model = model_fn(include_top=False)
+
+        input_tensor = tf.image.resize(self.image, INPUT_SHAPE[:2])
+        input_tensor = self._pre_process_image(input_tensor)
+
+        out = model(input_tensor, training=False)
+
+        self.assertTrue(isinstance(out, tf.Tensor))
+        self.assertEqual(len(out.shape), 4)
 
 
 if __name__ == "__main__":
