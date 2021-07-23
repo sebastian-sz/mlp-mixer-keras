3e724abfaf674b20fab4f998412e97686d6497a4
Sebastian <mocart15@gmail.com>
2021-07-23 14:22:14 +0200

    Updated and extended unit tests.

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,6 +1,6 @@
 import os
 import tempfile
-from typing import Callable, Tuple
+from typing import Callable
 
 import tensorflow as tf
 from absl.testing import absltest, parameterized
@@ -20,8 +20,8 @@ class TestModelsUnit(parameterized.TestCase):
     model_path = os.path.join(tempfile.mkdtemp(), "model.h5")
 
     @parameterized.named_parameters(TEST_PARAMS)
-    def test_model_inference(self, model_fn: Callable[[], tf.keras.Model]):
-        model = model_fn()  # TODO: arguments
+    def test_model_inference(self, model_fn: Callable):
+        model = model_fn(weights=None)
         mock_inputs = self.rng.uniform(shape=(1, *INPUT_SHAPE), dtype=tf.float32)
         outputs = model(mock_inputs, training=False)
 
@@ -29,8 +29,8 @@ class TestModelsUnit(parameterized.TestCase):
         self.assertEqual(outputs.shape, (1, 1000))
 
     @parameterized.named_parameters(TEST_PARAMS)
-    def test_model_keras_serialization(self, model_fn: Callable[[], tf.keras.Model]):
-        model = model_fn()  # TODO: arguments.
+    def test_model_keras_serialization(self, model_fn: Callable):
+        model = model_fn(weights=None)
         tf.keras.models.save_model(
             model=model, filepath=self.model_path, save_format="h5"
         )
@@ -43,6 +43,74 @@ class TestModelsUnit(parameterized.TestCase):
     def tearDown(self) -> None:
         if os.path.exists(self.model_path):
             os.remove(self.model_path)
+
+    @parameterized.named_parameters(TEST_PARAMS)
+    def test_models_not_being_created_with_invalid_input_shapes(
+        self, model_fn: Callable
+    ):
+        # Provided input_shape doesn't match pretrained input shape
+        with self.assertRaises(ValueError):
+            model_fn(input_shape=(160, 160, 3))
+
+        # Provided input_shape has invalid number of params:
+        with self.assertRaises(ValueError):
+            model_fn(input_shape=(1, 224, 224, 3), weights=None)
+
+        # Invalid number of channels
+        with self.assertRaises(ValueError):
+            model_fn(input_shape=(224, 224, 2), weights=None)  # Should be either 1 or 3
+
+        # Input shape is below minimum
+        with self.assertRaises(ValueError):
+            model_fn(input_shape=(8, 8, 3), weights=None)
+
+    @parameterized.named_parameters(TEST_PARAMS)
+    def test_models_work_with_smaller_input_shapes(self, model_fn: Callable):
+        input_shape = (160, 160, 3)
+        noise = self.rng.uniform(shape=(1, *input_shape))
+        model = model_fn(weights=None, input_shape=input_shape)
+
+        model(noise, training=False)
+
+    @parameterized.named_parameters(TEST_PARAMS)
+    def test_models_work_with_single_channel_input(self, model_fn: Callable):
+        input_shape = (160, 160, 1)
+        noise = self.rng.uniform(shape=(1, *input_shape))
+        model = model_fn(weights=None, input_shape=input_shape)
+
+        model(noise, training=False)
+
+    @parameterized.named_parameters(TEST_PARAMS)
+    def test_models_with_custom_head_sequential(self, model_fn: Callable):
+        num_classes = 10
+        backbone = model_fn(weights=None, include_top=False, pooling="avg")
+
+        new_model = tf.keras.Sequential([backbone, tf.keras.layers.Dense(num_classes)])
+
+        noise = self.rng.uniform(shape=(1, 224, 224, 3))
+        output = new_model(noise, training=False)
+
+        self.assertTrue(isinstance(output, tf.Tensor))
+        self.assertTrue(output.shape == (1, num_classes))
+
+    @parameterized.named_parameters(TEST_PARAMS)
+    def test_models_with_custom_head_functional(self, model_fn: Callable):
+        input_shape = (160, 160, 3)
+        num_classes = 10
+
+        # Make model:
+        backbone = model_fn(weights=None, include_top=False, input_shape=input_shape)
+        backbone.trainable = False
+        x = tf.keras.layers.GlobalAveragePooling2D()(backbone.output)
+        outputs = tf.keras.layers.Dense(num_classes)(x)
+        model = tf.keras.Model(inputs=[backbone.inputs], outputs=[outputs])
+
+        # Run inference
+        noise = self.rng.uniform(shape=(1, *input_shape))
+        output = model(noise, training=False)
+
+        self.assertTrue(isinstance(output, tf.Tensor))
+        self.assertEqual(output.shape, (1, 10))
 
 
 if __name__ == "__main__":
