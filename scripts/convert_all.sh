1c7fe825e8883c92c87bf8547dd99a2285eada83
Sebastian <mocart15@gmail.com>
2021-07-23 12:21:47 +0200

    Updated weight update scripts with include_top option.

--- scripts/convert_all.sh
+++ scripts/convert_all.sh
@@ -8,11 +8,23 @@ python scripts/mlp_mixer_weight_update_util.py \
   -m b16 \
   -o weights/mlp-mixer-b16.h5
 
+python scripts/mlp_mixer_weight_update_util.py \
+  -i weights/original_weights/b16-imagenet1k.npz \
+  -m b16 \
+  --notop \
+  -o weights/mlp-mixer-b16_notop.h5
+
 ### B16 SAM ###
 python scripts/mlp_mixer_weight_update_util.py \
   -i weights/original_weights/b16-sam.npz \
   -m b16 \
   -o weights/mlp-mixer-b16-sam.h5
+
+python scripts/mlp_mixer_weight_update_util.py \
+  -i weights/original_weights/b16-sam.npz \
+  -m b16 \
+  --notop \
+  -o weights/mlp-mixer-b16-sam_notop.h5
 
 ### B32 SAM ###
 python scripts/mlp_mixer_weight_update_util.py \
@@ -20,8 +32,20 @@ python scripts/mlp_mixer_weight_update_util.py \
   -m b32 \
   -o weights/mlp-mixer-b32-sam.h5
 
+python scripts/mlp_mixer_weight_update_util.py \
+  -i weights/original_weights/b32-sam.npz \
+  -m b32 \
+  --notop \
+  -o weights/mlp-mixer-b32-sam_notop.h5
+
 ### L16 Imagenet 1k ###
 python scripts/mlp_mixer_weight_update_util.py \
   -i weights/original_weights/l16-imagenet1k.npz \
   -m l16 \
   -o weights/mlp-mixer-l16.h5
+
+python scripts/mlp_mixer_weight_update_util.py \
+  -i weights/original_weights/l16-imagenet1k.npz \
+  -m l16 \
+  --notop \
+  -o weights/mlp-mixer-l16_notop.h5
